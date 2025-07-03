2ffc677a2dd55188f7770f586cde0853f56cac89
Arindam-Ghosh-1-Dev <arindamg987@gmail.com>
2025-07-03 09:45:00 +0530

    Create math_identities.c

---
+++ math_identities.c
@@ -0,0 +1,17 @@
+// Basic Mathematical identity and trigonometric identity
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a,b,c;
+    char name[20] = 'Arindam Ghosh';
+    printf("%c", name);
+    a=tan(30);
+    printf("%.2f", a);
+    scanf("%f", &c);
+    b=sin(c);
+    printf("%.2f\n", b);
+    return 0;
+}
